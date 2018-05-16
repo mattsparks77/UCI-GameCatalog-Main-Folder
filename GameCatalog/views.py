d595e05c7286f21c7a51775de64f214990b7b294
Whitney Tran <whitnet@uci.edu>
2018-05-15 18:49:24 -0700

    Remade a new project, replaced other files
    The initial push did not have the files that were created when you first
    started a new project via command prompt. So I made a new project and
    followed the tutorials from the youtube playlist linked on Discord.

--- GameCatalog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the GameCatalog index.\n WE ARE THE CREATORS WE SLAY THE GAMES: Plus Denenburg \n Matt Sparks \n Daniel Williams \n Whitney Tran \n David Wong")
