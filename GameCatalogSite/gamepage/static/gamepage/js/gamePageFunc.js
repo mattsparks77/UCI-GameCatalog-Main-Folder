777568b7962c7d8f5f2e0b60bd2ea109f45f773e
Whitney Tran <whitnet@uci.edu>
2018-05-24 21:11:54 -0700

    Text Changes, Spacing, Tweaking Title Resizing

---
+++ GameCatalogSite/gamepage/static/gamepage/js/gamePageFunc.js
@@ -0,0 +1,5 @@
+$(function() {
+    while( $('#fitin div').height() > $('#fitin').height() ) {
+        $('#fitin div').css('font-size', (parseInt($('#fitin div').css('font-size')) - 1) + "px" );
+    }
+});
