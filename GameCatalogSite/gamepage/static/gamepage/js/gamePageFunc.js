cdd9efb5da25b45d4738a7a163d354cce05df58c
Whitney Tran <whitnet@uci.edu>
2018-05-25 22:43:35 -0700

    GamePage fiddling, added game info to the left

--- GameCatalogSite/gamepage/static/gamepage/js/gamePageFunc.js
+++ GameCatalogSite/gamepage/static/gamepage/js/gamePageFunc.js
@@ -1,3 +1,11 @@
+var offset = 80;
+
+$('.navbar li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+});
+
 $(function() {
     while( $('#fitin div').height() > $('#fitin').height() ) {
         $('#fitin div').css('font-size', (parseInt($('#fitin div').css('font-size')) - 1) + "px" );
